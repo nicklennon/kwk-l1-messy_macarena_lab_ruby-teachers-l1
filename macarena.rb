221c615737a4a171ce1cbc4329514a830f9f01da
nicklennon <nicklennon.53@gmail.com>
2018-06-12 21:51:03 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -5,6 +5,16 @@ puts ""
 # Every line should have one instruction using puts.
 # The first instruction is correct. 
 
+#1. "Right arm out in front of you, palm facing down."#
+#2. "Left arm out in front of you, palm facing down."
+#3. "Turn your right palm up."
+#4. "Turn your left palm up."
+#5. "Place your right hand on your left shoulder."
+#6. "Place your left hand on your right shoulder."
+#7. "Bring your right hand up and around to the back of your head."
+8. "Bring your left hand up and around to the back your head."
+#
+
 # Step 1: GOOD
 puts "Step 1: Right arm out in front of you, palm facing down."
 
